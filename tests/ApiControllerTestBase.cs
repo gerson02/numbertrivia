771db360c49422f4bd223e67ae6455fbc0774c64
gerson <gerson02@gmail.com>
2018-09-17 08:12:48 -0400

    Second Commit

---
+++ tests/ApiControllerTestBase.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Http.Abstractions;
+
+namespace tests
+{
+    public class ApiControllerTestBase
+    {
+        protected HttpClient GetClient()
+        {
+            var builder = new WebHostBuilder()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseEnvironment("Testing");
+
+            var server = new TestServer(builder)                ;
+            var client = server.CreateClient();
+
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+    }
+}
